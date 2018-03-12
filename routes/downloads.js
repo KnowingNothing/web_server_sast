8206882b42ebb488e74112b76217e881d25203b2
Ir1d <sirius.caffrey@gmail.com>
2018-03-12 18:40:43 +0800

    feat: enable download submitted files

--- routes/downloads.js
+++ routes/downloads.js
@@ -6,6 +6,7 @@ let check = require('../public/javascripts/check');
 
 let admin_cfg = config.admin;
 let guest_cfg = config.guest;
+let store_path = config.store_path;
 
 let pool_admin = mysql.createPool(admin_cfg);
 let pool_guest = mysql.createPool(guest_cfg);
@@ -33,6 +34,33 @@ router.get('/', function(req, res){
             }
     });
     }
+});
+
+router.get('/contests', function (req, res) {
+  let id = req.query.id;
+  let file_name = req.query.file_name;
+  let ip_info = req.headers.remoteip || req.socket.remoteAddress;
+  let reg = /^[\d]+$/;
+  if(!reg.test(id))
+  {
+      res.redirect('/login');
+  }
+  else
+  {
+      now = new Date();
+      timestamp = now.getTime();
+      check.check_already_login(id, ip_info, timestamp, false, function(already, valid){
+          if(!already || !valid)
+          {
+              res.redirect('/login');
+          }
+          else
+          {
+            if (file_name === undefined) res.redirect('/login');
+            res.download(store_path + filename);
+          }
+  });
+  }
 });
 
 module.exports = router;
